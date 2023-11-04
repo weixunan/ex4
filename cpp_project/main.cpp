b606bb989080308e194c90111d1759821882339f
arisu <yan@yan.com>
2023-11-04 11:43:57 +0800

    2021152028commit

--- cpp_project/main.cpp
+++ cpp_project/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 // 分支里的main文件
 int main()
 {
-	string s="2021152027韦旭南，是计算机与软件学院的一名学生，今年大三了";
+	string s="2021152027韦旭南，是计算机与软件学院的一名学生，今年大三了2021152028";
     PrintName p(s);
 	p.printFun();
 	system("pause");
